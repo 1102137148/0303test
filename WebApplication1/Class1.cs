8209fba673966528ff4b802893ef55b0009fcf80
1102137148 <1102137148@gm.kuas.edu.tw>
2016-03-03 15:00:45 +0800

    happy

---
+++ WebApplication1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Happy");
+            System.Console.ReadLine();
+        }
+    }
+}
